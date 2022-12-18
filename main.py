987c6952df113454d83bff514e770652680a2a4d
Abdul Ahad Rauf <120673653+AbdulAhadRauf@users.noreply.github.com>
2022-12-18 15:05:26 +0000

    head tails

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello world")
+import random
+for i in range(10):
+  # seed=random.seed(1.9999*int(input("seed")))
+  n = random.randint(1, 2)
+  if n == 1:
+    print("Heads")
+  else:
+    print("Tails")
+  print(n)
